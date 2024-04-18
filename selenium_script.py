37545ca935b8eb6c65a833d4a12b2423da965336
Rani Maklada <rani_m.14@hotmail.com>
2024-04-18 05:35:33 +0300

    Update selenium_script.py

--- selenium_script.py
+++ selenium_script.py
@@ -1,29 +1,26 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 def main():
-    # Set up Chrome options
     options = Options()
-    options.add_argument("--headless")  # Runs Chrome in headless mode.
-    options.add_argument("--no-sandbox")  # Bypass OS security model
-    options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    # Specify the path to ChromeDriver. You can also set this path in your system's PATH environment variable.
-    chrome_driver_path = '/usr/local/bin/chromedriver'
-
-    # Create a new instance of Chrome
-    driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
+    # Get the path to ChromeDriver from environment variable
+    chrome_driver_path = os.getenv('CHROME_DRIVER', '/usr/local/bin/chromedriver')
+    
+    # Create a Service object with the path to ChromeDriver
+    s = Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=s, options=options)
 
     try:
-        # Navigate to a website
         driver.get("http://example.com")
-
-        # Take a screenshot and save it to the current directory
         driver.save_screenshot("screenshot.png")
         print("Screenshot taken and saved as 'screenshot.png'.")
-
     finally:
-        # Clean up: close the browser window
         driver.quit()
 
 if __name__ == "__main__":
