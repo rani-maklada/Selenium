92a83ebfffe9b5b553e636fe96b1835e47f9d326
Rani Maklada <rani_m.14@hotmail.com>
2024-04-18 06:12:50 +0300

    Update selenium_script.py

--- selenium_script.py
+++ selenium_script.py
@@ -4,26 +4,28 @@ from selenium.webdriver.chrome.options import Options
 import os
 
 def main():
-    # Set the path for ChromeDriver and Chrome Binary
-    chrome_driver_path = os.getenv('CHROMEWEBDRIVER')
-    chrome_binary_path = os.getenv('CHROME_BIN_PATH', '/opt/google/chrome/chrome')
-
+    # Set up Chrome options
     options = Options()
-    options.binary_location = chrome_binary_path  # Specify the path to Chrome binary
     options.add_argument("--headless")  # Runs Chrome in headless mode.
-    options.add_argument("--no-sandbox")  # Bypass OS security model, necessary for Docker
+    options.add_argument("--no-sandbox")  # Bypass OS security model
     options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+    options.binary_location = os.getenv('CHROME_BIN_PATH') + "/chrome"
 
-    # Set up the Chrome WebDriver with the specified driver and options
+    # Set the path to the ChromeDriver
+    chrome_driver_path = os.getenv('CHROMEWEBDRIVER')
+
+    # Create a new instance of Chrome
     service = Service(executable_path=chrome_driver_path)
     driver = webdriver.Chrome(service=service, options=options)
 
     try:
         # Navigate to a website
         driver.get("http://example.com")
+
         # Take a screenshot and save it to the current directory
         driver.save_screenshot("screenshot.png")
         print("Screenshot taken and saved as 'screenshot.png'.")
+
     finally:
         # Clean up: close the browser window
         driver.quit()
