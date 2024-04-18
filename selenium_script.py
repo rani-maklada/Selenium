5f2a60936b58f7d347655d38d8ed9b3afafb80fe
Rani Maklada <rani_m.14@hotmail.com>
2024-04-18 06:05:23 +0300

    Update selenium_script.py

--- selenium_script.py
+++ selenium_script.py
@@ -1,34 +1,32 @@
-import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import os
 
-def main(chrome_path, driver_path):
-    # Set up Chrome options
+def main():
+    # Set the path for ChromeDriver and Chrome Binary
+    chrome_driver_path = os.getenv('CHROMEWEBDRIVER', '/usr/local/bin/chromedriver')
+    chrome_binary_path = os.getenv('CHROME_BIN_PATH', '/opt/google/chrome/chrome')
+
     options = Options()
-    options.binary_location = chrome_path  # Path from command line
+    options.binary_location = chrome_binary_path  # Specify the path to Chrome binary
     options.add_argument("--headless")  # Runs Chrome in headless mode.
-    options.add_argument("--no-sandbox")  # Bypass OS security model
+    options.add_argument("--no-sandbox")  # Bypass OS security model, necessary for Docker
     options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
 
-    # Create a new instance of Chrome
-    service = Service(executable_path=driver_path)
+    # Set up the Chrome WebDriver with the specified driver and options
+    service = Service(executable_path=chrome_driver_path)
     driver = webdriver.Chrome(service=service, options=options)
 
     try:
         # Navigate to a website
         driver.get("http://example.com")
-
         # Take a screenshot and save it to the current directory
         driver.save_screenshot("screenshot.png")
         print("Screenshot taken and saved as 'screenshot.png'.")
-
     finally:
         # Clean up: close the browser window
         driver.quit()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python selenium_script.py <chrome_binary_path> <chromedriver_path>")
-        sys.exit(1)
-    main(sys.argv[1], sys.argv[2])
+    main()
