a1eb70139aa4d2aaaa4548813f4acbe4b5b0d4e1
Rani Maklada <rani_m.14@hotmail.com>
2024-04-21 02:54:39 +0300

    Update selenium_script.py

--- selenium_script.py
+++ selenium_script.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import os
+import argparse
 
-def main():
-    # Get the paths from environment variables
-    chrome_driver_path = os.environ.get('CHROMEDRIVER')  # Ensure this matches Jenkins
-    chrome_binary_path = os.environ.get('CHROME_BINARY')  # Ensure this matches Jenkins
-
+def main(chrome_driver_path, chrome_binary_path):
+    # chrome_driver_path = os.environ.get('CHROMEDRIVER')  # Ensure this matches Jenkins
+    # chrome_binary_path = os.environ.get('CHROME_BINARY')  # Ensure this matches Jenkins
     print("Chrome Driver Path:", chrome_driver_path)
     print("Chrome Binary Path:", chrome_binary_path)
 
@@ -26,11 +25,15 @@ def main():
         driver.get("https://www.example.com")
         print(driver.title)
         # Save a screenshot to the workspace
-        screenshot_path = os.path.join(os.getcwd(), 'screenshot.png')  # Saving to the current working directory
+        screenshot_path = os.path.join(os.getcwd(), 'screenshot.png')
         driver.save_screenshot(screenshot_path)
         print(f"Screenshot saved to {screenshot_path}")
     finally:
         driver.quit()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Open Selenium")
+    parser.add_argument("--chrome_driver", required=True, help="Path to Chrome driver")
+    parser.add_argument("--chrome_binary_path", required=True, help="Path to Chrome binary")
+    args = parser.parse_args()
+    main(args.chrome_driver, args.chrome_binary_path)
